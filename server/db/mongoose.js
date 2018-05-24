31d45c5add4a9f813362c61a5e0c42ac22b3569b
David Pinhasik <davidpinhasik@gmail.com>
2018-05-24 13:15:08 +0300

    Added use of MONGODB_URI in connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://david:S@deena#9@ds115131.mlab.com:15131/todo-app-api' ||  'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
