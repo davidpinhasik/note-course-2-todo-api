0ba398beddc1dd33f0edf86c549667765d1a39e0
David Pinhasik <davidpinhasik@gmail.com>
2018-05-29 15:33:07 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt      = require('jsonwebtoken');
+const bcrypt   = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$jR9882Zznlt7qJ0e027ygO8WQjiIL7gRxndWiwwXT5NvYOUyh/VDi';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+
+})
+
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // jwt.verify
 // var message = 'I am user number 3';
@@ -27,7 +44,7 @@ console.log('decoded', decoded);
 //   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
 // };
 //
-// // middle attempt to change Data
+ // // middle attempt to change Data
 // // token.data.id = 5;
 // // token.hash = SHA256(JSON.stringify(data)).toString()
 //
